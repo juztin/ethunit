d5027ff8e06fb625bfcc644d5c8756bdac3730c6
Justin Wilson <justin@velvet.blue>
2021-09-16 10:33:21 -0600

    Correct unit name in converions func

--- conversions.go
+++ conversions.go
@@ -10,6 +10,6 @@ func GweiToWei(i *big.Int) *big.Float { return ConvertInt(i, Gwei, Wei) }
 
 func GweiToEth(i *big.Int) *big.Float { return ConvertInt(i, Gwei, Ether) }
 
-func EthToWei(i *big.Int) *big.Float { return ConvertInt(i, Ether, Wei) }
+func EtherToWei(i *big.Int) *big.Float { return ConvertInt(i, Ether, Wei) }
 
-func EthToGwei(i *big.Int) *big.Float { return ConvertInt(i, Ether, Gwei) }
+func EtherToGwei(i *big.Int) *big.Float { return ConvertInt(i, Ether, Gwei) }
